386cebc393d5b2414d65f85be2542db30de45b43
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-25 20:23:30 +0300

    Change order of the buttons

--- src/components/Create/PricePicker/index.js
+++ src/components/Create/PricePicker/index.js
@@ -6,9 +6,9 @@ import NumbersRow from './NumbersRow';
 import TotalPrice from './TotalPrice';
 
 const numbers = [
-  ['9', '8', '7'],
-  ['6', '5', '4'],
-  ['3', '2', '1'],
+  ['7', '8', '9'],
+  ['4', '5', '6'],
+  ['1', '2', '3'],
   ['0', ',']
 ];
 
