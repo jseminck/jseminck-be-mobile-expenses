c9525e28c23d4c877a8bd5ac99f2d1b41a4a00c3
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-09-05 08:16:54 +0300

    Update test and add update script

--- src/components/Login/__tests__/LoginInput.js
+++ src/components/Login/__tests__/LoginInput.js
@@ -5,9 +5,26 @@ import LoginInput from './../LoginInput';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('renders correctly without password', () => {
     const tree = renderer.create(
-        <LoginInput />
+        <LoginInput
+            icon="glass"
+            placeholder="myPlaceholder"
+            onChangeText={() => {}}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly with password', () => {
+    const tree = renderer.create(
+        <LoginInput
+            icon="myIcon"
+            placeholder="myPlaceholder"
+            password={true}
+            onChangeText={() => {}}
+        />
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
